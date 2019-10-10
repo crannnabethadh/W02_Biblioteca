9ac01fd02f0758e91e0ec0b8c3ec01ab5399e73f
crannabethadh <crannabethadh@outlook.com>
2019-10-11 01:56:41 +0200

    First commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Biblioteca Windows Forms
+
+## learning our first UI
+
+[https://drive.google.com/open?id=1Dj1WEodwJi-t7gUfJF3SSVvVb4rO6koJPlDcts0t0qM](https://drive.google.com/open?id=1Dj1WEodwJi-t7gUfJF3SSVvVb4rO6koJPlDcts0t0qM)
+
---
+++ W02_BibliotecaWinformUI/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_BibliotecaWinformUI
+{
+    public class Book
+    {
+        public int ID_LLIB { get; set; }
+        //public string TITOL { get; set; }
+        public string TITOL { get; set; }
+        public string FullInfo
+        {
+            get
+            {
+                return ID_LLIB + ":\t" + TITOL;
+            }
+        }
+
+    }
+}
